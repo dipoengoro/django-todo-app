4053be16b3a828bc8ae9ec66ee59109f108f5634
Muhammad Diponegoro <juhyuko@gmail.com>
2025-07-20 16:20:59 +0700

    refactor: Implement singleton manager and soft delete logic

--- todo/views.py
+++ todo/views.py
@@ -3,71 +3,82 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
 
-class TodoListView(View):
-    def get(self, request):
-        """Menangani GET request untuk menampilkan semua tugas."""
-        with open('db.json', 'r') as f:
-            todos = json.load(f)
-        return render(request, 'todo/index.html', {'todos': todos})
+class TodoManager:
+    def __init__(self, filepath='db.json'):
+        self.filepath = filepath
 
-    def post(self, request):
-        """Menangani POST request dari form 'Add'."""
-        print("Data POST yang diterima:", request.POST)
-        with open('db.json', 'r') as f:
-            todos = json.load(f)
+    def _read_todos(self):
+        try:
+            with open(self.filepath, 'r') as f:
+                return json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
 
-        new_task_text = request.POST.get('task_name')
+    def _write_todos(self, todos):
+        with open(self.filepath, 'w') as f:
+            json.dump(todos, f, indent=2)
+
+    def get_all(self):
+        all_todos = self._read_todos()
+        return [item for item in all_todos if not item.get('is_deleted', False)]
+
+    def add(self, task_name):
+        todos = self._read_todos()
         new_id = max(item['id'] for item in todos) + 1 if todos else 1
         new_todo = {
             'id': new_id,
-            'task': new_task_text,
-            'done': False
+            'task': task_name,
+            'done': False,
+            'is_deleted': False,
         }
-
         todos.append(new_todo)
-        with open('db.json', 'w') as f:
-            json.dump(todos, f, indent=2)
+        self._write_todos(todos)
 
-        return redirect(reverse('todo_list'))
-
-class TodoToggleDoneView(View):
-    def post(self, request, todo_id):
-        with open('db.json', 'r') as f:
-            todos = json.load(f)
-
+    def toggle(self, todo_id):
+        todos = self._read_todos()
         for item in todos:
             if item['id'] == todo_id:
                 item['done'] = not item['done']
                 break
+        self._write_todos(todos)
 
-        with open('db.json', 'w') as f:
-            json.dump(todos, f, indent=2)
+    def update(self, todo_id, new_task):
+        todos = self._read_todos()
+        for item in todos:
+            if item['id'] == todo_id:
+                item['task'] = new_task
+                break
+        self._write_todos(todos)
 
+    def soft_delete(self, todo_id):
+        todos = self._read_todos()
+        for item in todos:
+            if item['id'] == todo_id:
+                item['is_deleted'] = True
+                break
+        self._write_todos(todos)
+
+todo_manager = TodoManager()
+class TodoListView(View):
+    def get(self, request):
+        todos = todo_manager.get_all()
+        return render(request, 'todo/index.html', {'todos': todos})
+
+    def post(self, request):
+        todo_manager.add(request.POST.get('task_name'))
+        return redirect(reverse('todo_list'))
+
+class TodoToggleDoneView(View):
+    def post(self, request, todo_id):
+        todo_manager.toggle(todo_id)
         return redirect(reverse('todo_list'))
 
 class TodoEditView(View):
     def post(self, request, todo_id):
-        with open('db.json', 'r') as f:
-            todos = json.load(f)
-
-        new_task_text = request.POST.get('task_name')
-        for item in todos:
-            if item['id'] == todo_id:
-                item['task'] = new_task_text
-                break
-
-        with open('db.json', 'w') as f:
-            json.dump(todos, f, indent=2)
-
+        todo_manager.update(todo_id, request.POST.get('task_name'))
         return redirect(reverse('todo_list'))
 
 class TodoDeleteView(View):
     def post(self, request, todo_id):
-        with open('db.json', 'r') as f:
-            todos = json.load(f)
-
-        todos_to_keep = [item for item in todos if item['id'] != todo_id]
-        with open('db.json', 'w') as f:
-            json.dump(todos_to_keep, f, indent=2)
-
+        todo_manager.soft_delete(todo_id)
         return redirect(reverse('todo_list'))
